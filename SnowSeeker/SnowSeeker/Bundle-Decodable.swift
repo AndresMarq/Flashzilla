6f5ca89cc0a18adcf01bcf187ca1fb0261ca718e
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2021-10-22 23:59:18 -0600

    Fixed bugs

---
+++ SnowSeeker/SnowSeeker/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  SnowSeeker
+//
+//  Created by Andres Marquez on 2021-09-08.
+//
+
+import Foundation
---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,8 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Andres Marquez on 2021-09-08.
+//
+
+import Foundation
